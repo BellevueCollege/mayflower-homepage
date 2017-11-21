32b012ffbf6c6c6fa20d7d9d69fa05d34047b444
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2017-11-21 11:46:07 -0800

    Strip out code to support Mayflower Lite
    Not a use case for Mayflower Homepage

---
+++ footer.php
@@ -0,0 +1,21 @@
+<?php
+if ( ! is_front_page() ) { ?>
+			</div><!-- row -->
+		</div><!-- col-md-12 -->
+<?php } ?>
+	</div><!-- #main .container -->
+</div><!-- #main-wrap -->
+
+<?php
+bc_footer();
+wp_footer();
+?>
+
+<!-- <?php
+$mayflower_version = wp_get_theme();
+echo $mayflower_version->Name . " version " . $mayflower_version->Version;
+?>  -->
+
+</body>
+</html>
+
