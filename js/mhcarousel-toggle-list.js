8f09e746d3f7bcbb0257790c13e8ea2791a96a58
Angela Vu <angela.vu@bellevuecollege.edu>
2018-01-10 12:40:38 -0800

    Add close toggle to button and clean up CSS

--- js/mhcarousel-toggle-list.js
+++ js/mhcarousel-toggle-list.js
@@ -3,6 +3,7 @@ jQuery(function() {
     jQuery(".list-group-tree").on('click', "[data-toggle=collapse]", function(){
         jQuery(this).toggleClass('in')
         jQuery(this).next(".list-group.collapse").collapse('toggle');
+        jQuery("#list-group-glyph").toggleClass('glyphicon-th-list glyphicon-remove');
         return false;
     })
 });
