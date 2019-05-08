29cf209e8720d91df004463170493cf189b310a2
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2019-05-08 11:14:08 -0700

    📦 NEW: Open Graph Support
    Fix MAYHP-35, as well as porting over the same fix as MAYF-84.
    
    Also update header code to better match the main Mayflower header code.

--- header.php
+++ header.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <?php
-	global $mayflower_options;
+global $post,
+	   $mayflower_options,
+	   $globals_version,
+	   $globals_url,
+	   $globals_path,
+	   $mayflower_brand,
+	   $mayflower_brand_css,
+	   $mayflower_theme_version;
+
+if ( ! ( is_array( $mayflower_options ) ) ) {
 	$mayflower_options = mayflower_get_options();
-	global $globals_version;
-	global $globals_url;
-	global $globals_path;
-	global $mayflower_brand;
-	global $mayflower_brand_css;
-	global $mayflower_theme_version;
-	$mayflower_theme_version = wp_get_theme();
+}
+
+$mayflower_theme_version = wp_get_theme();
+$post_meta_data = get_post_custom( $post->ID );
 ?>
 <!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="ie6"> <![endif]-->
 <!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="ie7"> <![endif]-->
@@ -19,19 +25,51 @@
 <!--<![endif]-->
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	<?php if ( isset ( $post_meta_data['_seo_meta_description'][0] ) ) { ?>
-		<meta name="description" content="<?php echo $post_meta_data['_seo_meta_description'][0]; ?>" />
+
+	<?php if ( isset( $post_meta_data['_seo_meta_description'][0] ) ) { ?>
+		<meta property="og:title" content="<?php echo esc_html( $post_meta_data['_seo_custom_page_title'][0] ); ?>" />
+	<?php } else { ?>
+		<meta property="og:title" content="<?php echo get_the_title() . ' :: ' . get_bloginfo( 'name', 'display' ) . ' @ Bellevue College' ?>" />
 	<?php } ?>
-	<?php if (isset($post_meta_data['_seo_meta_description'][0])) { ?>
-		<meta name="keywords" content="<?php echo $post_meta_data['_seo_meta_keywords'][0]; ?>" />
+
+	<?php if ( isset( $post_meta_data['_seo_meta_description'][0] ) ) { ?>
+		<meta name="description" content="<?php echo esc_html( $post_meta_data['_seo_meta_description'][0] ); ?>" />
+		<meta property="og:description" content="<?php echo esc_html( $post_meta_data['_seo_meta_description'][0] ); ?>" />
+	<?php } ?>
+	<?php if ( isset( $post_meta_data['_seo_meta_keywords'][0] ) ) { ?>
+		<meta name="keywords" content="<?php echo esc_html( $post_meta_data['_seo_meta_keywords'][0] ); ?>" />
 	<?php } ?>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/img/bellevue.ico" />
+	<link rel="icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/bellevue.ico" />
 	<!--[if IE]>
 		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/bellevue.ico" />
 	<![endif]-->
-	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<?php if ( is_archive( $post->ID ) ) { ?>
+		<meta name="robots" content="noindex, follow">
+	<?php } ?>
+	<link rel="profile" href="https://gmpg.org/xfn/11" />
+
+	<!--- Open Graph Tags -->
+	
+	<?php if ( 'post' === get_post_type() ) : ?>
+		<meta property="og:type" content="article" />
+		<meta property="article:published_time" content="<?php echo get_the_date( 'c' ) ?>" />
+		<meta property="article:modified_time" content="<?php echo get_the_modified_date( 'c' ) ?>" />
+		
+	<?php else : ?>
+		<meta property="og:type" content="website" />
+	<?php endif; ?>
+
+	<?php if ( get_the_post_thumbnail_url( get_the_ID(), 'medium' ) ) : ?>
+		<meta property="og:image" content="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'medium' ) ?>" />
+	<?php else: ?>
+		<meta property="og:image" content="https://www.bellevuecollege.edu/bc-og-default.jpg" />
+	<?php endif; ?>
+
+	<meta property="og:url" content="<?php echo get_permalink() ?>" />
+	<meta property="og:site_name" content="Bellevue College" />
+
 
 	<?php wp_head();
 
@@ -39,18 +77,18 @@
 
 	$post_top_parent_id = 0; //if needed, this ID is set to the top parent of this post
 	//set $post_top_parent_id and $is_main_site for later use (ignore error404 page)
-	if ( !(is_404())) {
-		if ($post->post_parent!="0"){
+	if ( ! ( is_404() ) ) {
+		if ( $post->post_parent!="0" ){
 			//this page has a parent
-			if(intval($post->post_parent)>0)
-			{
-				while(intval($post->post_parent)>0)
-					$post = get_post($post->post_parent);
+			if ( intval( $post->post_parent ) > 0 ) {
+				while ( intval( $post->post_parent) > 0 ) {
+					$post = get_post( $post->post_parent );
+				}
 			}
 			$post_top_parent_id = $post->ID;  //now we now the top parent
 		}
 	}
-	echo '<!-- parentid= '. $post_top_parent_id . '-->';
+	echo '<!-- parentid= ' . $post_top_parent_id . '-->';
 
 	?>
 </head>
@@ -89,7 +127,7 @@
 			<div id="main">
 
 
-	<?php } else if ( is_404() ) { ?>
+	<?php } elseif ( is_404() ) { ?>
 		<div id="main-wrap" class="<?php echo $mayflower_brand_css; ?>">
 			<div id="main" class="container no-padding">
 	<?php } else { ?>
